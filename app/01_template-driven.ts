5d59a67d6d7385e0037dc8f6c432458b86f9693e
Vince Bloise <vbloise3@verizon.net>
2017-01-16 14:15:16 -0500

    Initial commit

---
+++ app/01_template-driven.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by vincebloise on 1/16/17.
+ */
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app',
+    template: `
+    <form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
+      <div>Username:        <input type="text"     name="username" ngModel></div>
+      <div>SSN:             <input type="text"     name="ssn"      ngModel></div>
+      <div ngModelGroup="passwordsGroup">
+          <div>Password:        <input type="password" name="password" ngModel></div>
+          <div>Confirm password: <input type="password" name="pconfirm"  ngModel></div>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  `
+})
+class AppComponent {
+    onSubmit(formData) {
+        console.log(formData);
+    }
+}
+
+@NgModule({
+    imports     : [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent ],
+    bootstrap   : [ AppComponent ]
+})
+class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ systemjs.config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by vincebloise on 1/16/17.
+ */
+System.config({
+    transpiler: 'typescript',
+    typescriptOptions: {emitDecoratorMetadata: true},
+    map: {
+        '@angular': 'node_modules/@angular',
+        'rxjs'    : 'node_modules/rxjs'
+    },
+    paths: {
+        'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+    },
+    meta: {
+        '@angular/*': {'format': 'cjs'}
+    },
+    packages: {
+        'app'                              : {main: '01_template-driven', defaultExtension: 'ts'},
+        'rxjs'                             : {main: 'Rx'},
+        '@angular/core'                    : {main: 'core.umd.min.js'},
+        '@angular/common'                  : {main: 'common.umd.min.js'},
+        '@angular/compiler'                : {main: 'compiler.umd.min.js'},
+        '@angular/forms'                   : {main: 'forms.umd.min.js'},
+        '@angular/platform-browser'        : {main: 'platform-browser.umd.min.js'},
+        '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
+    }
+});
